747518c22f888ba08e876ee004f389770463fc3a
hikari <tanimoto0709@gmail.com>
2021-09-19 16:18:56 +0900

    add: 即時関数、条件演算子、論理演算子

---
+++ 06/src/App.js
@@ -0,0 +1,29 @@
+const Hello = () => {
+  const isReact = false;
+
+  // 即時関数えお利用することでJSX内にif文を書くことができる
+  return (
+    <p>
+      {(() => {
+        if (isReact) {
+          return 'Hello! React!';
+        } else {
+          return 'Hello!';
+        }
+      })()}
+    </p>
+  );
+
+  // {}で囲むとJSX内に式を記述できる
+  // return <p>{ isReact ? 'Hello! React!' : 'Hello!' }</p>;
+
+  // 論理積演算子 && 条件がtrueの時だけ値を返す
+  //return <p>{isReact && 'Hello! React!'}</p>;
+
+  // 論理和演算子 || 条件がfalseの時だけ値を返す
+  //return <p>{isReact || 'Hello! React!'}</p>;
+};
+
+export default function App() {
+  return <Hello />
+};
