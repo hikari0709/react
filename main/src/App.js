13481ba2290841a2ed707f3f661957fdf8b18754
hikari <tanimoto0709@gmail.com>
2021-09-30 09:38:31 +0900

    update: 副作用関数の宣言とuseEffectの利用

--- main/src/App.js
+++ main/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Counter } from './Counter';
 import { Hello } from './Hello';
 
@@ -8,6 +8,17 @@ const initialName = 'JavaScript';
 export default function App() {
   const [count, setCount] = useState(initialCount);
   const [name, setName] = useState(initialName);
+
+  // 副作用関数の宣言
+  const callbackFunction = () => {
+    document.title = `${count}回クリックされました`;
+
+    return () => {
+      console.log('hoge');
+    }
+  }
+
+  useEffect(callbackFunction, [count]);
 
   const countIncrement = () => setCount((prevCount) => prevCount + 1);
   const countDecrement = () => setCount((prevCount) => prevCount - 1);
